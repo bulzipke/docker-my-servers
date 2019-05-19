0c0261dc2d93e5419b66c766fc54e20cb9bf9b84
bulzipke <bulzipke@naver.com>
2019-05-20 00:59:18 +0900

    add more APK

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:latest
 MAINTAINER bulzipke <bulzipke@naver.com>
 
 RUN apk update && apk upgrade
-RUN apk add openjdk8-jre-base python3 python3-dev ffmpeg
+RUN apk add openjdk8-jre-base python3 python3-dev ffmpeg g++
 
 COPY rootfs /
 
--- rootfs/run_servers.py
+++ rootfs/run_servers.py
@@ -5,14 +5,16 @@ CURRENT_DIR = '.'
 
 print('start my servers')
 
+os.system('pip3 install --upgrade pip'.format(target))
 for target in [name for name in os.listdir(CURRENT_DIR) if os.path.isdir(os.path.join(CURRENT_DIR, name))]:
     for file in os.listdir('./{}'.format(target)):
         if file.endswith('.py'):
             print('run {} in {}'.format(file, target))
-            os.system('cd {}; pip3 install --upgrade pip -r requirements.txt'.format(target))
+            os.system('cd {}; pip3 install --upgrade -r requirements.txt'.format(target))
             os.system('cd {}; nohup python3 {} &'.format(target, file))
         elif file.endswith('.jar'):
             print('run {} in {}'.format(file, target))
             os.system('cd {}; nohup java -jar {} &'.format(target, file))
+
 print('complete')
 
