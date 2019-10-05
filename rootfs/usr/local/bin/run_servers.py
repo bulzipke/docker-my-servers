b5112f2d5c9416ed0a32924a18bed5bd2e44a994
bulzipke <bulzipke@naver.com>
2019-10-06 01:35:35 +0900

    Update run_servers.py

--- rootfs/usr/local/bin/run_servers.py
+++ rootfs/usr/local/bin/run_servers.py
@@ -6,14 +6,13 @@ TARGET_DIR = '/data'
 print('start my servers')
 
 for target in [name for name in os.listdir(TARGET_DIR) if os.path.isdir(os.path.join(TARGET_DIR, name))]:
-    print('test [{}] : '.format(target))
-    for file in os.listdir('./{}'.format(target)):
+    for file in os.listdir('{}/{}'.format(TARGET_DIR, target)):
         if file.endswith('.py'):
             print('run {} in {}'.format(file, target))
-            os.system('cd {}; nohup python3 -u {} &'.format(target, file))
+            os.system('cd {}/{}; nohup python3 -u {} &'.format(TARGET_DIR, target, file))
         elif file.endswith('.jar'):
             print('run {} in {}'.format(file, target))
-            os.system('cd {}; nohup java -jar {} &'.format(target, file))
+            os.system('cd {}/{}; nohup java -jar {} &'.format(TARGET_DIR, target, file))
 
 print('complete')
 
