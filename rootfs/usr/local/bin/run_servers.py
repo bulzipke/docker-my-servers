0a3deb3976ea1baeae4cbf5b34cd8fa5fbeecab9
bulzipke <bulzipke@naver.com>
2019-10-08 16:27:23 +0900

    Update run_servers.py

--- rootfs/usr/local/bin/run_servers.py
+++ rootfs/usr/local/bin/run_servers.py
@@ -6,6 +6,8 @@ TARGET_DIR = '/data'
 print('start my servers')
 
 for target in [name for name in os.listdir(TARGET_DIR) if os.path.isdir(os.path.join(TARGET_DIR, name))]:
+    if target == 'scripts':
+        continue
     for file in os.listdir('{}/{}'.format(TARGET_DIR, target)):
         if file.endswith('.py'):
             print('run {} in {}'.format(file, target))
